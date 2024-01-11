bc22cf98c31d71e895bb84cdf9492581d6e52420
atrox3d <atrox3d@gmail.com>
2024-01-11 06:54:53 +0100

    add twoliner

--- write-number-in-expanded-form/excercise.py
+++ write-number-in-expanded-form/excercise.py
@@ -1,5 +1,6 @@
 '''
-You will be given a number and you will need to return it as a string in Expanded Form. For example:
+You will be given a number and you will need to return it as a string in 
+Expanded Form. For example:
 
 expanded_form(12) # Should return '10 + 2'
 expanded_form(42) # Should return '40 + 2'
@@ -14,48 +15,60 @@ def expanded_form(num):
         '''
         strnum = str(num)
         reverse_powers_of_ten = [10**p for p in range(len(strnum))][::-1]
-        digits = []
+        expands = []
         for char, power in zip(strnum, reverse_powers_of_ten):
             if digit := int(char):
-                digits.append(digit*power)
-        result = ' + '.join(map(str, digits))
+                expands.append(digit*power)
+        result = ' + '.join(map(str, expands))
         return result
     
     def int_version(num):
         '''
         decompose number as integer
         '''
-        digits = []
+        expands = []
         power = 1
         while num:
             m = num % 10
             num = num // 10
             if m:
-                digits.append(m*power)
+                expands.append(m*power)
             power *= 10
-        return ' + '.join(map(str, digits[::-1]))
+        return ' + '.join(map(str, expands[::-1]))
     
     def string_enumerate_version(num):
+        '''
+        decompose number as string
+        compute powers of 10 via enumerate
+        '''
+        expands = []
+        for p, n in enumerate(str(num)[::-1]):
+            digit = 10**p * int(n)
+            if digit:
+                expands.append(digit)
+        return ' + '.join(map(str, expands[::-1]))
+    
+    def int_enumerate_version(num):
         '''
         decompose number as integer
         compute powers of 10 via enumerate
         '''
-        digits = []
-        for p, n in enumerate(str(num)[::-1]):
-            digit = 10**p * int(n)
-            if digit:
-                digits.append(digit)
-        return ' + '.join(map(str, digits[::-1]))
-    
-    def int_enumerate_version(num):
-        e = enumerate((num%10, num:=num//10) for n in range(len(str(num))))
-        digits = []
-        for power, (number, _) in list(e)[::-1]:
+        pow_num_rest = enumerate((num%10, num:=num//10) for n in range(len(str(num))))
+        expands = []
+        for power, (number, _) in list(pow_num_rest)[::-1]:
             digit = number * 10**power
             if digit:
-                digits.append(digit)
-        return ' + '.join(map(str, digits))
+                expands.append(digit)
+        return ' + '.join(map(str, expands))
 
+    def twoliner(num):
+        '''
+        same as above, less readable, just for fun
+        '''
+        pow_num_rest = enumerate((num%10, num:=num//10) for n in range(len(str(num))))
+        digits = [str(number * 10**power) for power, (number, _) in pow_num_rest if number][::-1]
+        return ' + '.join(digits)
+    
 
     def autotest(num):
         '''
@@ -65,9 +78,12 @@ def expanded_form(num):
         iv = int_version(num)
         sev = string_enumerate_version(num)
         iev = int_enumerate_version(num)
-        assert sv == iv == sev == iev, \
-            ValueError(f'{sv!r} != {iv!r} != {sev!r} != {iev!r}')
-        return iev
+        tl = twoliner(num)
+        assert sv == iv == sev == iev == tl, \
+            ValueError(f'{sv!r} != {iv!r} != {sev!r} != {iev!r} != {tl!r}')
+        
+        # return whichever you like
+        return tl
 
     return autotest(num)
 
