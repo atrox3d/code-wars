d2748ee0470309d6c0ef0f674c815296e7396edf
atrox3d <atrox3d@gmail.com>
2024-01-11 05:17:29 +0100

    add int version

--- write-number-in-expanded-form/excercise.py
+++ write-number-in-expanded-form/excercise.py
@@ -8,23 +8,43 @@ NOTE: All numbers will be whole numbers greater than 0.
 '''
 
 def expanded_form(num):
-    print(f'{num = }')
     def string_version(num):
+        '''
+        decompose number as string
+        '''
         strnum = str(num)
-        ps = list([10**p for p in range(len(strnum))])[::-1]
+        reverse_powers_of_ten = [10**p for p in range(len(strnum))][::-1]
         digits = []
-        for n, p in zip(strnum, ps):
-            if n := int(n):
-                digits.append(n*p)
-        print(digits)
+        for char, power in zip(strnum, reverse_powers_of_ten):
+            if digit := int(char):
+                digits.append(digit*power)
         result = ' + '.join(map(str, digits))
-        print(result)
         return result
     
-    return string_version(num)
+    def int_version(num):
+        print(f'{num = }')
+        digits = []
+        power = 1
+        while num:
+            m = num % 10
+            num = num // 10
+            print(f'{m, num, power = }')
+            if m:
+                digits.append(m*power)
+            power *= 10
+        print(f'{digits = }')
+        
+        return ' + '.join(map(str, digits[::-1]))
 
+
+    sv = string_version(num)
+    iv = int_version(num)
+
+    assert sv == iv, ValueError(f'{sv!r} != {iv!r}')
+
+    return iv
 
 
 
 for n in 12, 42, 70304:
-    expanded_form(n)
+    print(expanded_form(n))
