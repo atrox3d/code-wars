3beb779d3456d97ecc11515ee452c218edf828b3
atrox3d <atrox3d@gmail.com>
2024-01-12 15:46:11 +0100

    finish

--- first_non_repeating_letter/exercise.py
+++ first_non_repeating_letter/exercise.py
@@ -13,16 +13,36 @@ initial letter. For example, the input 'sTreSS' should return 'T'.
 If a string contains all repeating characters, 
 it should return an empty string ("") or None -- see sample tests.
 '''
-def first_non_repeating_letter(s):
-    print(f'{s = }')
-    return s
+def first_non_repeating_letter(s: str):
+    from collections import Counter
+    
+    counter = Counter(s.lower())
+
+    # empty input
+    if not s:
+        return ''
+
+    # just a repetition
+    if len(counter) == 1 and len(s) > 1:
+        return ''
+    
+    uniques = [k for k, v in counter.items() if v==1]
+    if not uniques:
+        return ''
+
+    for position, char in enumerate(s.lower()):
+        if char in uniques:
+            result = s[position]
+            return result
 
 
 if __name__ == '__main__':
     tests = dict(
-                moonmen='r',
+                moonmen='e',
                 aaa='',
-                sTreSS='T'
+                sTreSS='T',
+                abba='',
+                a='a',
     )
     for value, expected in tests.items():
         result = first_non_repeating_letter(value)
