88d76e31d1a8e41cf71056e6d2bb86711ac69f87
atrox3d <atrox3d@gmail.com>
2024-01-12 17:57:04 +0100

    linked list

--- recover-a-secret-string-from-random-triplets/exercise.py
+++ recover-a-secret-string-from-random-triplets/exercise.py
@@ -17,13 +17,56 @@ deduce the original string.
 In particular, this means that the secret string will never contain 
 letters that do not occur in one of the triplets given to you.
 '''
+def explore_matrix(triplets):
+    for y, triplet in enumerate(triplets):
+        print(triplet, '*')
+        search = triplets[:y] + triplets[y+1:]
+        # for _ in search:
+        #     print(_)
+        # print()
+        for x, char in enumerate(triplet):
+            line: list
+            for line in search:
+                if char in line:
+                    print(char, line, line.index(char))
+
+def linked_list(triplets):
+    from dataclasses import dataclass
+    @dataclass
+    class Node:
+        value: str
+        prev: 'Node' = None
+        next: 'Node' = None
+    
+    root = None
+    for y, triplet in enumerate(triplets):
+        for x, char in enumerate(triplet):
+            if not root:
+                root = Node(char)
+                node = root
+            else:
+                node = root
+                while node.value != char and node.next:
+                    node = node.next
+                if node.value != char:
+                    node.next = Node(char)
+                    node.next.prev = root
+
+    node = root
+    values = []
+    while node:
+        values.append(node.value)
+        node = node.next
+    print(values)
 
 def recoverSecret(triplets):
-  '''
-  triplets is a list of triplets from the secrent string. 
-  Return the string.
-  '''
-  pass
+    '''
+    triplets is a list of triplets from the secrent string. 
+    Return the string.
+    '''
+    explore_matrix(triplets)            
+    linked_list(triplets)
+        
 
 if __name__ == '__main__':
     secret = "whatisup"
