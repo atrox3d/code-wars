04f6e1e889b002ada5bb8af5d14823d385e7a6bf
atrox3d <atrox3d@gmail.com>
2024-01-12 07:07:16 +0100

    almost working

--- range-extraction/try2.py
+++ range-extraction/try2.py
@@ -3,17 +3,17 @@ def solution(data):
     buffer = []
     prev = None
     for current in data:
-        if prev is None:
-            buffer.append(current)
-        else:
-            if current - prev == 1:
-                buffer.append(current)
-            else:
-                if len(buffer) >= 3:
-                    result.append(buffer)
-                else:
-                    result.extend(buffer)
-                    result.append(current)
+        if prev is None:                    # first element, first loop
+            buffer.append(current)          # stash it
+        else:                               # all the other loops
+            if current - prev == 1:         # do we have a sequence?
+                buffer.append(current)      # stash it if yes
+            else:                           # not in sequence
+                if len(buffer) >= 3:        # do we have enough elements?
+                    result.append(buffer)   # append sub list
+                else:                       # not enough elements
+                    result.extend(buffer)   # add elements as single
+                    # result.append(current)  # * append current?
                 result.append(current)
                 buffer = []
         prev = current
