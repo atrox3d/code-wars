a1615b8b0e9efd6fc58543b805ca65c2840d369a
atrox3d <atrox3d@gmail.com>
2024-01-12 07:19:16 +0100

    format the result

--- range-extraction/try2.py
+++ range-extraction/try2.py
@@ -1,20 +1,42 @@
 def solution(data):
-    result = []
-    buffer = []
-    prev = None
-    for current in data:
-        if prev is None:                    # first element, first loop
-            buffer.append(current)          # stash it
-        else:                               # all the other loops
-            if current - prev == 1:         # do we have a sequence?
-                buffer.append(current)      # stash it if yes
-            else:                           # not in sequence
-                if len(buffer) >= 3:        # do we have enough elements?
-                    result.append(buffer)   # append sub list
-                else:                       # not enough elements
-                    result.extend(buffer)   # add elements as single
-                    # result.append(current)  # * append current?
-                result.append(current)
-                buffer = []
-        prev = current
+    def create_result(data):
+        result = []
+        buffer = []
+        prev = None
+        for current in data:
+            if prev is None:                    # first element, first loop
+                buffer.append(current)          # stash it
+            else:                               # all the other loops
+                if current - prev == 1:         # do we have a sequence?
+                    buffer.append(current)      # stash it if yes
+                else:                           # not in sequence
+                    if len(buffer) >= 3:        # do we have enough elements?
+                        result.append(buffer)   # append sub list
+                    else:                       # not enough elements
+                        result.extend(buffer)   # add elements as single
+                        # result.append(current)  # * append current?
+                    result.append(current)      # * append current?
+                    buffer = []                 # empty buffer
+            prev = current                      # save prev
+        return result
+    
+    def format_result(result):
+        format_list = []
+        for current in result:
+            if isinstance(current, list):
+                format_list.append(
+                    f'{current[0]}-{current[-1]}'
+                )
+            elif isinstance(current, int):
+                format_list.append(str(current))
+            else:
+                t = type(current)
+                raise TypeError(f'unknown type {t} for {current}')
+        
+        return ','.join(format_list)
+
+    result = create_result(data)
     print(f'{result = }')
+    format_list = format_result(result)
+    print(f'{format_list = }')
+    return format_list
