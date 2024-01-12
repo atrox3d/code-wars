4ba7c402b4ca2906107bdf1480a68bdc6c137850
atrox3d <atrox3d@gmail.com>
2024-01-12 10:39:31 +0100

    add graph

--- memoized-fibonacci/excercise.py
+++ memoized-fibonacci/excercise.py
@@ -1,11 +1,25 @@
-def fibonacci(n):
-    print(f'{n = }')
+r'''
+                     5
+                ____/ \___    
+              /            \
+             4              3
+          /     \          /   \
+        3        2        2     1
+      /   \     / \      / \
+    2      1   1   0    1   0
+ /    \
+1       0
+
+'''
+
+def fibonacci(n, branch: str):
+    print(f'{branch:10}: {n = }')
     if n in [0, 1]:
         return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
+    return fibonacci(n - 1, 'left') + fibonacci(n - 2, 'right')
 
 def main():
-    result = fibonacci(10)
+    result = fibonacci(5, 'root')
     print(f'{result = }')
 
 if __name__ == '__main__':
