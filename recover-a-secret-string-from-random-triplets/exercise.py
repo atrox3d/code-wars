4051ade3f930c3fff33b1c1fb486e3bf5e878eeb
atrox3d <atrox3d@gmail.com>
2024-01-13 15:57:45 +0100

    first working solution

--- recover-a-secret-string-from-random-triplets/exercise.py
+++ recover-a-secret-string-from-random-triplets/exercise.py
@@ -64,19 +64,24 @@ def recoverSecret(triplets: list[list]):
     triplets is a list of triplets from the secrent string. 
     Return the string.
     '''
+    import time
+    import logging
+
+    logging.basicConfig(level='DEBUG')
+    logger = logging.getLogger('test')
     # explore_matrix(triplets)            
     # linked_list(triplets)
     # print(*triplets)
-    # flatten = [i for l in triplets for i in l]
-    # print(f'{flatten = }')
+    flatten = [i for l in triplets for i in l]
+    print(f'{flatten = }')
     # uniques = list(set(flatten))
     uniques = list(dict.fromkeys(flatten))
     print(f'{uniques = }')
-    done = False
+    # done = False
     # exit()
-    while not done:
+    # while not done:
         # ready to quit loop
-        done = True
+        # done = True
         # flatten = ['t', 'u', 'p', 'w', 'h', 'i', 't', 's', 'u', 'a', 't', 's', 'h', 'a', 'p', 't', 'i', 's', 'w', 'h', 's']
         # unique =  ['t', 'u', 'p', 'w', 'h', 'i', 's', 'a']
         # triplets = [
@@ -89,41 +94,85 @@ def recoverSecret(triplets: list[list]):
         # ['w','h','s']
         # ]
     
+    def to_str(l:list) -> str:
+        return ''.join(l)
+    
     # loop through unique chars
     modified = False
     done = False
     while not done:
         done = True
-        for unique_idx in range(len(uniques)):
-                # for each char in unique
-                unique_char = uniques[unique_idx]
-                # scan triplets
-                for triplet in triplets:
-                    # check only triplets containing char
-                    if unique_char in triplet:
-                        # get position of unique char in triplet
-                        triplet_idx = triplet.index(unique_char)
-                        try:
-                            # try to get previous char in triplet
-                            triplet_prev_char = triplet[triplet_idx - 1]
-                            # check the relative position inside uniques
-                            prev_idx = uniques.index(triplet_prev_char)
-                            # prev char should be before current char
-                            if prev_idx > unique_idx:
-                                # change place
-                                uniques.remove(triplet_prev_char)
-                                uniques.insert(
-                                    unique_idx -1,
-                                    triplet_prev_char
-                                )
-                                modified = True
-                                done = False
-                                break
-                        except IndexError:
-                            pass                    
-                if modified:
-                    break
-    print(f'{uniques = }')
+        print(f'while | uniques = {to_str(uniques)}')
+        time.sleep(2)
+        for idx in range(len(uniques)):
+            # for each char in unique
+            char = uniques[idx]
+            print(f'    {char, idx = }')
+            # scan triplets
+            for triplet in triplets:
+                print(f'        triplet = {to_str(triplet)}')
+                # check only triplets containing char
+                if char in triplet:
+                    # get position of unique char in triplet
+                    for triplet_ndx, triplet_char in enumerate(triplet):
+                        control = triplet.index(char)
+                        print(f'            {char, control = }')
+                        if triplet_char != char:
+                            where = uniques.index(triplet_char)
+                            print(f'            {triplet_char, where = }')
+                            if triplet_ndx < control:
+                                if where > idx:
+                                    print(f'            ***move*** {triplet_char}')
+                                    uniques.remove(triplet_char)
+                                    uniques.insert( idx, triplet_char )
+                                    modified = True
+                                    done = False
+                                    break
+                            else:
+                                if where < idx:
+                                    print(f'            ***move*** {triplet_char}')
+                                    uniques.remove(triplet_char)
+                                    uniques.insert( idx +1, triplet_char )
+                                    modified = True
+                                    done = False
+                    # end for triplet
+            # end for idx
+            print(f'end for | uniques = {to_str(uniques)}')
+            print()
+            print()
+    return to_str(uniques)
+                # exit()
+                                
+                        # try:
+                        #     # try to get previous char in triplet
+                        #     triplet_next_char = triplet[triplet_unique_idx + 1]
+                        #     # check the relative position inside uniques
+                        #     next_idx = uniques.index(triplet_next_char)
+                        #     # prev char should be before current char
+                        #     print(f'{            triplet_next_char, next_idx =}')
+                        #     if next_idx < unique_idx:
+                        #         # change place
+                        #         print(f'         move {triplet_next_char}')
+                        #         uniques.remove(triplet_next_char)
+                        #         uniques.insert(
+                        #             unique_idx -1,
+                        #             triplet_next_char
+                        #         )
+                        #         modified = True
+                        #         done = False
+                        #         break
+                        # except IndexError:
+                        #     logger.exception('IndexError')
+                        #     uniques.insert(
+                        #         unique_idx,
+                        #         triplet_next_char
+                        #     )
+                        #     modified = True
+                        #     done = False
+                        #     break                                    
+    #             if modified:
+    #                 break
+    # print(f'END {uniques = }')
 
 
 if __name__ == '__main__':
