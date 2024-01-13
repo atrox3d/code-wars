b4dc1e57efff029f296ea84d864b628600b61bf5
atrox3d <atrox3d@gmail.com>
2024-01-13 16:02:52 +0100

    remove printlogging

--- recover-a-secret-string-from-random-triplets/exercise.py
+++ recover-a-secret-string-from-random-triplets/exercise.py
@@ -38,37 +38,28 @@ def recoverSecret(triplets: list[list]):
         for idx in range(len(uniques)):
             # for each char in unique
             char = uniques[idx]
-            print(f'    {char, idx = }')
             # scan triplets
             for triplet in triplets:
-                print(f'        triplet = {to_str(triplet)}')
                 # check only triplets containing char
                 if char in triplet:
                     # get position of unique char in triplet
                     for triplet_ndx, triplet_char in enumerate(triplet):
                         control = triplet.index(char)
-                        print(f'            {char, control = }')
                         if triplet_char != char:
                             where = uniques.index(triplet_char)
-                            print(f'            {triplet_char, where = }')
                             if triplet_ndx < control:
                                 if where > idx:
-                                    print(f'            ***move*** {triplet_char}')
                                     uniques.remove(triplet_char)
                                     uniques.insert( idx, triplet_char )
                                     done = False
                                     break
                             else:
                                 if where < idx:
-                                    print(f'            ***move*** {triplet_char}')
                                     uniques.remove(triplet_char)
                                     uniques.insert( idx +1, triplet_char )
                                     done = False
                     # end for triplet
             # end for idx
-            print(f'end for | uniques = {to_str(uniques)}')
-            print()
-            print()
     return to_str(uniques)
 
 if __name__ == '__main__':
