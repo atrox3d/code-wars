a90180fdeedb0c3b169f18ba7c144d02f529c7aa
atrox3d <atrox3d@gmail.com>
2024-01-13 15:59:56 +0100

    clean old comments

--- recover-a-secret-string-from-random-triplets/exercise.py
+++ recover-a-secret-string-from-random-triplets/exercise.py
@@ -17,48 +17,6 @@ deduce the original string.
 In particular, this means that the secret string will never contain 
 letters that do not occur in one of the triplets given to you.
 '''
-def explore_matrix(triplets):
-    for y, triplet in enumerate(triplets):
-        print(triplet, '*')
-        search = triplets[:y] + triplets[y+1:]
-        # for _ in search:
-        #     print(_)
-        # print()
-        for x, char in enumerate(triplet):
-            line: list
-            for line in search:
-                if char in line:
-                    print(char, line, line.index(char))
-
-def linked_list(triplets):
-    from dataclasses import dataclass
-    @dataclass
-    class Node:
-        value: str
-        prev: 'Node' = None
-        next: 'Node' = None
-    
-    root = None
-    for y, triplet in enumerate(triplets):
-        for x, char in enumerate(triplet):
-            if not root:
-                root = Node(char)
-                node = root
-            else:
-                node = root
-                while node.value != char and node.next:
-                    node = node.next
-                if node.value != char:
-                    node.next = Node(char)
-                    node.next.prev = root
-
-    node = root
-    values = []
-    while node:
-        values.append(node.value)
-        node = node.next
-    print(values)
-
 def recoverSecret(triplets: list[list]):
     '''
     triplets is a list of triplets from the secrent string. 
@@ -69,30 +27,10 @@ def recoverSecret(triplets: list[list]):
 
     logging.basicConfig(level='DEBUG')
     logger = logging.getLogger('test')
-    # explore_matrix(triplets)            
-    # linked_list(triplets)
-    # print(*triplets)
     flatten = [i for l in triplets for i in l]
     print(f'{flatten = }')
-    # uniques = list(set(flatten))
     uniques = list(dict.fromkeys(flatten))
     print(f'{uniques = }')
-    # done = False
-    # exit()
-    # while not done:
-        # ready to quit loop
-        # done = True
-        # flatten = ['t', 'u', 'p', 'w', 'h', 'i', 't', 's', 'u', 'a', 't', 's', 'h', 'a', 'p', 't', 'i', 's', 'w', 'h', 's']
-        # unique =  ['t', 'u', 'p', 'w', 'h', 'i', 's', 'a']
-        # triplets = [
-        # ['t','u','p'],
-        # ['w','h','i'],
-        # ['t','s','u'],
-        # ['a','t','s'],
-        # ['h','a','p'],
-        # ['t','i','s'],
-        # ['w','h','s']
-        # ]
     
     def to_str(l:list) -> str:
         return ''.join(l)
@@ -141,39 +79,6 @@ def recoverSecret(triplets: list[list]):
             print()
             print()
     return to_str(uniques)
-                # exit()
-                                
-                        # try:
-                        #     # try to get previous char in triplet
-                        #     triplet_next_char = triplet[triplet_unique_idx + 1]
-                        #     # check the relative position inside uniques
-                        #     next_idx = uniques.index(triplet_next_char)
-                        #     # prev char should be before current char
-                        #     print(f'{            triplet_next_char, next_idx =}')
-                        #     if next_idx < unique_idx:
-                        #         # change place
-                        #         print(f'         move {triplet_next_char}')
-                        #         uniques.remove(triplet_next_char)
-                        #         uniques.insert(
-                        #             unique_idx -1,
-                        #             triplet_next_char
-                        #         )
-                        #         modified = True
-                        #         done = False
-                        #         break
-                        # except IndexError:
-                        #     logger.exception('IndexError')
-                        #     uniques.insert(
-                        #         unique_idx,
-                        #         triplet_next_char
-                        #     )
-                        #     modified = True
-                        #     done = False
-                        #     break                                    
-    #             if modified:
-    #                 break
-    # print(f'END {uniques = }')
-
 
 if __name__ == '__main__':
     secret = "whatisup"
