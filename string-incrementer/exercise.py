f7ba0a784d8fb99a5f828f0c9bd3cfff89e10eb0
atrox3d <atrox3d@gmail.com>
2024-01-14 05:40:34 +0100

    testing regexes

--- string-incrementer/exercise.py
+++ string-incrementer/exercise.py
@@ -1,9 +1,10 @@
 '''
 Your job is to write a function which increments a string, to create a new string.
 
-If the string already ends with a number, the number should be incremented by 1.
-If the string does not end with a number. the number 1 should be appended to 
-the new string.
+If the string already ends with a number, 
+    the number should be incremented by 1.
+If the string does not end with a number. 
+the number 1 should be appended to the new string.
 
 Examples:
 
@@ -22,18 +23,59 @@ considered.
 '''
 def increment_string(strng):
     import re
-    exp = r'([a-z]+[0-9]*[a-z]+)([0-9+]*)'
-    parts = re.match(exp, strng)
-    str
-    print(strng, parts, parts.groups())
-    return parts
+    
+    # exp = r'([a-z]+[0-9]*[a-z]+)([0-9+]*)'
+    
+    # exp = r'(.+(?!.*\d))([0-9+]*)'
+    # exp = r'(.+)(?!.*\d)(\d*)'
+    # exp = r'(.*)(?:\D|^)(\d+)'
+    # exp = r'(\d+)\D*$'
+    # exp = r'^(.*)(\d*)$'
+
+    exp = r'(.*)(?<=\D)(\d+)'
+    
+    print(f'{strng = }')
+    print(f'{exp   = }')
+    match = re.match(exp, strng)
+    if match:
+        # print(f'{match.groups() = }')
+        # print(f'{type(match.groups()) = }')
+        # print(f'{len(match.groups()) = }')
+
+        str_part, int_part = match.groups() 
+    else:
+        str_part, int_part = ("", "")
+
+    print(f'{str_part = }')
+    print(f'{int_part = }')
+
+    len_int_part = len(int_part)
+    int_part = int(int_part) if int_part else 0
+    result = f'{str_part}{int_part + 1:0{len_int_part}}'
+    print(f'------->{result = }')
+    return result
 
 if __name__ == '__main__':
-        assert increment_string("foobar001") == "foobar002"
-        assert increment_string("fo99obar99") == "fo99obar100"
-        assert increment_string("") == "1"
-        assert increment_string("foo") == "foo1"
-        assert increment_string("foobar1") == "foobar2"
-        assert increment_string("foobar00") == "foobar01"
-        assert increment_string("foobar99") == "foobar100"
-        assert increment_string("foobar099") == "foobar100"
+    tests = [
+        ("foobar001", "foobar002"),
+        ("foobar1", "foobar2"),
+        ("foobar00", "foobar01"),
+        ("foobar99", "foobar100"),
+        ("foobar099", "foobar100"),
+        ("fo99obar99", "fo99obar100"),
+        ("", "1"),
+        ("30UiN959672340582688^][28{'F064)NLDC'59\v\"R::PL61800000000087637", 
+            "30UiN959672340582688^][28{'F064)NLDC'59\v\"R::PL61800000000087638"),
+        ("foo", "foo1"),
+    ]
+    for _input, expected in tests:
+        try:
+            print(f'running increment_string({_input})')
+            res = increment_string(_input)
+            assert res == expected, f'{res!r} != {expected!r}'
+            print('[               OK                  ]')
+        except AssertionError as ae:
+            print(repr(ae))
+            # exit()
+        finally:
+            print()
