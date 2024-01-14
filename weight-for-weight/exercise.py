585eabc421b95aaad3f43f9dbe0dbbc4e6e7eb5d
atrox3d <atrox3d@gmail.com>
2024-01-14 08:25:08 +0100

    update template

--- weight-for-weight/exercise.py
+++ weight-for-weight/exercise.py
@@ -1,7 +1,3 @@
-'''
-exercise description
-'''
-
 def exercise_runner(func):
     def param2str(*args, **kwargs) -> str:
         params = list(map(str, args)) + [f'{k}={v}' for k, v in kwargs.items()]
@@ -18,16 +14,56 @@ def exercise_runner(func):
         return result
     return wrapper
 
-@exercise_runner
-def your_function_here(arg, kwarg):
-    return 'put function here'
+'''
+My friend John and I are members of the "Fat to Fit Club (FFC)". 
+John is worried because each month a list with the weights of members is 
+published and each month he is the last on the list which means he is 
+the heaviest.
+
+I am the one who establishes the list so I told him: 
+"Don't worry any more, I will modify the order of the list". 
+It was decided to attribute a "weight" to numbers. 
+The weight of a number will be from now on the sum of its digits.
+
+For example 99 will have "weight" 18, 100 will have "weight" 1 so 
+in the list 100 will come before 99.
+
+Given a string with the weights of FFC members in normal order 
+can you give this string ordered by "weights" of these numbers?
+
+Example:
+"56 65 74 100 99 68 86 180 90" ordered by numbers weights becomes: 
+
+"100 180 90 56 65 74 68 86 99"
+When two numbers have the same "weight", 
+let us class them as if they were strings (alphabetical ordering) 
+and not numbers:
+
+180 is before 90 since, having the same "weight" (9), 
+it comes before as a string.
+
+All numbers in the list are positive numbers and the list can be empty.
+
+Notes
+it may happen that the input string have leading,
+trailing whitespaces and more than a unique whitespace between two 
+consecutive numbers
+For C: The result is freed.
+'''
+
+def your_function_here(*args, **kwargs):
+    pass
 
 def main():
+    runner = exercise_runner(your_function_here)
     tests = [
         ('input', 'expected')
     ]
     for input, expected in tests:
-        result = your_function_here(input, kwarg='kwarg')
-        assert result == expected, f'{result} != {expected}'
+        result = runner(input)
+        try:
+            assert result == expected, f'{result} != {expected}'
+        except AssertionError as ae:
+            print(repr(ae))
 if __name__ == '__main__':
     main()
