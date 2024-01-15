9d0815360442b40bd57bfce557678b841633c9b1
atrox3d <atrox3d@gmail.com>
2024-01-15 18:33:19 +0100

    move functions to v1

--- battleship-field-validator/exercise.py
+++ battleship-field-validator/exercise.py
@@ -26,43 +26,7 @@ neither by edge nor by corner.
 This is all you need to solve this kata. 
 If you're interested in more information about the game, visit this link.
 '''
-def find_ships(field: list[list], direction: str):
-    ships = []
-    ship = False
-
-    outer_range = range(len(field[0])) if direction == 'v' else range(len(field))
-    inner_range = range(len(field)) if direction == 'v' else range(len(field[0]))
-
-    for outer in outer_range:
-        for inner in inner_range:
-            if direction == 'v':
-                value = field[inner][outer]
-                coords = inner, outer
-            else:
-                value = field[outer][inner]
-                coords = outer, inner
-            if value and not ship:
-                ship = True
-                new_ship = [coords]
-            elif value and ship:
-                new_ship.append(coords)
-            elif ship and not value:
-                if len(new_ship) > 1:
-                    ships.append(new_ship)
-                    ship = []
-                ship = False
-    return ships
-
-def check_overlapping_ships(hships: list[list], vships: [list[list]]):
-    print(f'{hships = }')
-    print(f'{vships = }')
-    vflatten = [coord for i in vships for coord in i]
-    print(f'{vflatten = }')
-    for ship in hships:
-        for coord in ship:
-            if coord in vflatten:
-                return False
-    return True
+from v1 import find_ships, check_overlapping_ships
 
 def validate_battlefield(field):
     # write your magic here
