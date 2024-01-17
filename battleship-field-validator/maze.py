dba826e8c518056574c30832d5d985de0f6163d1
atrox3d <atrox3d@gmail.com>
2024-01-17 15:34:26 +0100

    format with border

--- battleship-field-validator/maze.py
+++ battleship-field-validator/maze.py
@@ -24,6 +24,16 @@ def convert_maze_to_type(maze, convert_type):
 def replace_maze_items(maze, charmap):
     return [[charmap.get(c, c) for c in row] for row in maze]
 
+def format_maze_join(maze, join):
+    return [join.join(map(str, row)) for row in maze]
+
+def format_maze_border(maze, join=' '):
+    width = len(maze[0])
+    top = bottom = ['+' + '-+' * width]
+    rows = []
+    rows = [f'|{join.join(map(str, row))}|' for row in maze]
+    return top + rows + bottom
+
 def print_maze(maze: list[list]):
     for row in maze:    
         print(row)
@@ -39,6 +49,9 @@ def bfs(maze, path=""):
 
 maze = load_maze()
 maze = convert_maze_to_type(maze, convert_type=int)
-maze = replace_maze_items(maze, {1: 'X', 0: ' '})
+maze = replace_maze_items(maze, {1: '#', 0: ' '})
+# maze = format_maze_join(maze, ' ')
+maze = format_maze_border(maze)
+
 print_maze(maze)
 # print_maze(maze)
