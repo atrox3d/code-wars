f5b81def804a97723fa7a540d8dc9938fda196f7
atrox3d <atrox3d@gmail.com>
2024-01-20 18:42:56 +0100

    display_maze

--- battleship-field-validator/recurse_maze/main.py
+++ battleship-field-validator/recurse_maze/main.py
@@ -1,4 +1,4 @@
-import csv
+import csv, os
 from pathlib import Path
 
 def load_csv_maze(filename: str):
@@ -6,9 +6,23 @@ def load_csv_maze(filename: str):
         reader = csv.reader(file)
         return [line for line in reader]
 
+def display_maze(maze, path):
+    m = maze[:]
+    os.system('clear')
+
+    draw = ''
+    for row in m:
+        for item in row:
+            item = str(item).replace('1', '#')
+            item = str(item).replace('0', ' ')
+            draw += item
+        draw += '\n'
+    
+    print(draw)
+
 def main():
     maze = load_csv_maze(Path(__file__).parent / 'maze.txt')
-    print(maze)
+    display_maze(maze, None)
 
 
 if __name__ == '__main__':
