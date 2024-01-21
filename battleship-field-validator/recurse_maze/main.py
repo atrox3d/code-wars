1db970a257fccff9da86f18547956359f2c3ad26
atrox3d <atrox3d@gmail.com>
2024-01-21 10:12:30 +0100

    change var names, add exit not found case

--- battleship-field-validator/recurse_maze/main.py
+++ battleship-field-validator/recurse_maze/main.py
@@ -2,14 +2,15 @@ import csv, os, time
 from pathlib import Path
 
 def load_csv_maze(filename: str):
-    with open(filename) as file:
+    with open(filename, newline='') as file:
         reader = csv.reader(file)
         grid =  [[int(char) if char in '01' else char for char in line] for line in reader ]
         return grid
 
-def display_maze(maze, path, wall=chr(9608), free=' ', print_path=True):
+def display_maze(maze, path, wall=chr(9608), free=' ', print_path=True, sleep=0.2):
     m2 = maze[:]
-    time.sleep(0.2)
+    if sleep:
+        time.sleep(sleep)
     os.system('clear')
 
     for y, x in path:
@@ -23,7 +24,7 @@ def display_maze(maze, path, wall=chr(9608), free=' ', print_path=True):
         print(f'{path = }')
 
     
-def move(maze, path):
+def move(maze, path, START='A', END='B', FREE=0, WALL=1, VISITED=2, sleep=0.2):
     ROWS = len(maze)
     COLS = len(maze[0])
     LEFT = UP = -1
@@ -31,29 +32,28 @@ def move(maze, path):
     DIRECTIONS = ((0, RIGHT), (DOWN, 0), (0, LEFT), (UP, 0))
 
     current = path[-1]
-    display_maze(maze, path)
+    display_maze(maze, path, sleep=sleep)
 
-    availables = [(y+dy, x+dx) for dy, dx in DIRECTIONS for y, x in (current,)]
-    possibles = [(y, x) for y, x in availables if 0 <= y < ROWS and 0 <= x < COLS]
-    legals = [(y, x) for y, x in possibles if maze[y][x] in ('A', 'B', 0)]
-    for item in legals:
-        y, x = item
-        if item in path: 
-            continue
-        elif maze[y][x] == 'B':
-            newpath = path + (item,)
+    new_coords = [(y+dy, x+dx) for dy, dx in DIRECTIONS for y, x in (current,)]
+    legal_coords = [(y, x) for y, x in new_coords if 0 <= y < ROWS and 0 <= x < COLS]
+    coords = [(y, x) for y, x in legal_coords if maze[y][x] in (START, END, FREE)]
+    for coord in [yx for yx in coords if yx not in path]:
+        y, x = coord
+        if maze[y][x] == 'B':
+            newpath = path + (coord,)
             display_maze(maze, path)
-            input('solution found, press enter to finish')
+            print('solution found')
             sys.exit()
         else:
-            newpath = path + (item,)
-            move(maze, newpath)
-            maze[y][x] = 2
+            newpath = path + (coord,)
+            move(maze, newpath, START, END, FREE, WALL)
+            maze[y][x] = VISITED
             display_maze(maze, path)
 
 def main():
     maze = load_csv_maze(Path(__file__).parent / 'maze.txt')
-    move(maze, ((1, 0),))
+    move(maze, ((1, 0),), sleep=0)
+    print('exit NOT FOUND!')
 
 if __name__ == '__main__':
     import sys
