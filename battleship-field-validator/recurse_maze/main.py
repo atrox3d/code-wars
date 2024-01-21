000ea9ed81ffe29b2ff43c700e10b8dc96fb9287
atrox3d <atrox3d@gmail.com>
2024-01-21 09:23:45 +0100

    add print path

--- battleship-field-validator/recurse_maze/main.py
+++ battleship-field-validator/recurse_maze/main.py
@@ -7,7 +7,7 @@ def load_csv_maze(filename: str):
         grid =  [[int(char) if char in '01' else char for char in line] for line in reader ]
         return grid
 
-def display_maze(maze, path, wall=chr(9608), free=' '):
+def display_maze(maze, path, wall=chr(9608), free=' ', print_path=True):
     m2 = maze[:]
     os.system('clear')
 
@@ -21,6 +21,9 @@ def display_maze(maze, path, wall=chr(9608), free=' '):
                  for item in row]) 
                 for row in m2])
             )
+    if print_path:
+        print(f'{path = }')
+
     
 def move(maze, path):
     time.sleep(0.2)
@@ -31,9 +34,7 @@ def move(maze, path):
     DIRECTIONS = ((0, RIGHT), (DOWN, 0), (0, LEFT), (UP, 0))
 
     cur = path[-1]
-    # os.system('clear')
     display_maze(maze, path)
-    print(f'{path = }')
 
     availables = [(y+dy, x+dx) for dy, dx in DIRECTIONS for y, x in (cur,)]
     possibles = [(y, x) for y, x in availables if 0 <= y < ROWS and 0 <= x < COLS]
