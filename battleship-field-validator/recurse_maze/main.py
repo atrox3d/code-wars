186551da26df2595a26926baa4196c98c0a3cb55
atrox3d <atrox3d@gmail.com>
2024-01-21 09:32:59 +0100

    optimize

--- battleship-field-validator/recurse_maze/main.py
+++ battleship-field-validator/recurse_maze/main.py
@@ -9,34 +9,31 @@ def load_csv_maze(filename: str):
 
 def display_maze(maze, path, wall=chr(9608), free=' ', print_path=True):
     m2 = maze[:]
+    time.sleep(0.2)
     os.system('clear')
 
     for y, x in path:
         m2[y][x] = '.'
     m2[y][x] = 'M'
 
-    print('\n'.join(
-            [''.join(
+    print('\n'.join([''.join(
                 [ str(item).replace('1', wall).replace('0', free).replace('2', free)
-                 for item in row]) 
-                for row in m2])
-            )
+                 for item in row]) for row in m2]))
     if print_path:
         print(f'{path = }')
 
     
 def move(maze, path):
-    time.sleep(0.2)
     ROWS = len(maze)
     COLS = len(maze[0])
     LEFT = UP = -1
     RIGHT = DOWN = 1
     DIRECTIONS = ((0, RIGHT), (DOWN, 0), (0, LEFT), (UP, 0))
 
-    cur = path[-1]
+    current = path[-1]
     display_maze(maze, path)
 
-    availables = [(y+dy, x+dx) for dy, dx in DIRECTIONS for y, x in (cur,)]
+    availables = [(y+dy, x+dx) for dy, dx in DIRECTIONS for y, x in (current,)]
     possibles = [(y, x) for y, x in availables if 0 <= y < ROWS and 0 <= x < COLS]
     legals = [(y, x) for y, x in possibles if maze[y][x] in ('A', 'B', 0)]
     for item in legals:
@@ -53,14 +50,10 @@ def move(maze, path):
             move(maze, newpath)
             maze[y][x] = 2
             display_maze(maze, path)
-            time.sleep(0.2)
-
 
 def main():
     maze = load_csv_maze(Path(__file__).parent / 'maze.txt')
-    # display_maze(maze, [])
     move(maze, ((1, 0),))
-
 
 if __name__ == '__main__':
     import sys
