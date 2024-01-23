be78c746adc7ebf4f20968db8faabdec1e2bc15d
atrox3d <atrox3d@gmail.com>
2024-01-23 04:35:41 +0100

    reformat

--- battleship-field-validator/solution2/helpers.py
+++ battleship-field-validator/solution2/helpers.py
@@ -2,13 +2,11 @@ import csv, os, time
 
 MAX_RECURSION_LEVEL = RECURSION_LEVEL = 0
 
-
 def load_csv_battlefield(filename: str):
     with open(filename, newline='') as file:
         reader = csv.reader(file)
         battlefield =  [[int(char.strip()) if char.strip() in '01' else char for char in line] for line in reader ]
         return battlefield
-
 
 def add_coordinates(matrix: list[list]) -> list[str]:
     top = bottom = ['  '] + [str(i)[-1] for i in range(len(matrix[0]))] + [' ']
@@ -21,14 +19,19 @@ def add_coordinates(matrix: list[list]) -> list[str]:
     out.append(bottom)
     return out
 
-def display(battlefield, path, ships, 
+def display(
+            battlefield, path, ships, 
             SCANNED='#', SHIP=chr(9608), FREE=' ', PATH='.', HEAD='M', 
-            print_matrix=False, print_path=False, sleep=0.2):
+            print_matrix=False, 
+            print_path=False, 
+            sleep=0.2
+            ):
     time.sleep(sleep)
     os.system('clear')
 
     print(f'{MAX_RECURSION_LEVEL = }')
     print(f'{RECURSION_LEVEL = }')
+    
     bf = battlefield[:]
     for y, x in path:
         bf[y][x] = PATH
@@ -38,9 +41,11 @@ def display(battlefield, path, ships,
     if print_matrix:
         for row in matrix:
             print(''.join(map(str, row)))
+    
     # print('+' + '-' * len(battlefield[0]) + '+')
     # print('\n'.join(['|' + ''.join([str(item).replace('1', SHIP).replace('0', FREE).replace('2', FREE).replace('3', SCANNED)for item in row]) + '|' for row in bf])
     # print('+' + '-' * len(battlefield[0]) + '+')
+    
     bf = [[str(item).replace('1', SHIP)
           .replace('0', FREE)
           .replace('2', FREE)
@@ -51,5 +56,6 @@ def display(battlefield, path, ships,
 
     for ship in ships:
         print(f'{ship = }')
+
     if print_path:
         print(f'{path = }')
