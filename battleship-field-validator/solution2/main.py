6e7a7efaa45e84f75dd6acea225903f41e25ec23
atrox3d <atrox3d@gmail.com>
2024-01-23 04:33:04 +0100

    mark lines for deletion

--- battleship-field-validator/solution2/main.py
+++ battleship-field-validator/solution2/main.py
@@ -28,43 +28,56 @@ def scan_ship(battlefield, path, y, x, FREE=0, SHIP=1, SCANNED=3):
 
     coords = get_coords(battlefield, (y, x), FREE, SHIP)
     for coord in [(y, x) for y, x in coords if (y, x) not in path and battlefield[y][x] == SHIP]:
-        log.debug(f'for loop: {coord =} {ship = }')
+
+        log.debug(f'for loop: {coord =} {ship = }') # DELETE
+
         y, x = coord
         battlefield[y][x] = SCANNED
         path = path + (coord, )
-        log.debug(f'RECURSE {coord = }')
-        log.debug(f'RECURSE {path = }')
+
+        log.debug(f'RECURSE {coord = }') # DELETE
+        log.debug(f'RECURSE {path = }') # DELETE
+
         block = scan_ship(battlefield, path, y, x)
         assert not any(block in ship for block in block), 'duplicate ship'
         ship.extend(block)
 
     log.debug(f'EXIT: {ship = }')
+
     return ship
         
 
 def explore(battlefield, path, ships=None, START='A', END='B', FREE=0, SHIP=1, VISITED=2, sleep=0.2):
-    helpers.MAX_RECURSION_LEVEL += 1
-    helpers.RECURSION_LEVEL += 1
+    helpers.MAX_RECURSION_LEVEL += 1 # DELETE
+    helpers.RECURSION_LEVEL += 1 # DELETE
     
     current = path[-1]
     ships = ships if ships is not None else []
-    display(battlefield, path, ships, sleep=sleep)
+    
+    display(battlefield, path, ships, sleep=sleep) # DELETE
+
     coords = get_coords(battlefield, current, FREE, SHIP)
     for coord in [yx for yx in coords if yx not in path]:
         y, x = coord
         if battlefield[y][x] == SHIP:
-            log.debug(f'ship found {coord = }')
+    
+            log.debug(f'ship found {coord = }') # DELETE
+    
             newpath = path + (coord,)
             ship = scan_ship(battlefield, newpath, y, x)
-            log.debug(f'{ship = }')
             ships.append(ship)
-            display(battlefield, path, ships)
+    
+            log.debug(f'{ship = }') # DELETE
+            display(battlefield, path, ships) # DELETE
+    
         else:
             newpath = path + (coord,)
             explore(battlefield, newpath, ships, START, END, FREE, SHIP)
             battlefield[y][x] = VISITED
-            display(battlefield, path, ships)
-    helpers.RECURSION_LEVEL -= 1
+    
+            display(battlefield, path, ships) # DELETE
+    
+    helpers.RECURSION_LEVEL -= 1 # DELETE
     assert len(set(path)) == len(path), "you have duplicate coordinates"
 
 def main():
