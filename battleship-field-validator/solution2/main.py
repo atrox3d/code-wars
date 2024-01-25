0c7b3841c226b259bd2ccec5dc31d4242d067d56
atrox3d <atrox3d@gmail.com>
2024-01-25 12:50:16 +0100

    remove coords

--- battleship-field-validator/solution2/main.py
+++ battleship-field-validator/solution2/main.py
@@ -2,13 +2,13 @@ from pathlib import Path
 
 import helpers
 
-def get_coords(battlefield, current, FREE, SHIP):
+def get_coords(battlefield, y, x, FREE, SHIP):
     ROWS = len(battlefield) 
     COLS = len(battlefield[0])
     LEFT = UP = -1
     RIGHT = DOWN = 1
     DIRECTIONS = ((0, RIGHT), (DOWN, 0), (0, LEFT), (UP, 0))
-    new_coords = [(y+dy, x+dx) for dy, dx in DIRECTIONS for y, x in (current,)]
+    new_coords = [(cy+dy, cx+dx) for dy, dx in DIRECTIONS for cy, cx in ((y, x),)]
     legal_coords = [(y, x) for y, x in new_coords if 0 <= y < ROWS and 0 <= x < COLS]
     coords = [(y, x) for y, x in legal_coords if battlefield[y][x] in [FREE, SHIP]]
     return coords
@@ -16,7 +16,7 @@ def get_coords(battlefield, current, FREE, SHIP):
 def scan_ship(battlefield, path, y, x, FREE=0, SHIP=1, SCANNED=3):
     battlefield[y][x] = SCANNED
     ship = [(y, x)]
-    for y, x in [(y, x) for y, x in get_coords(battlefield, (y, x), FREE, SHIP) if (y, x) not in path and battlefield[y][x] == SHIP]:
+    for y, x in [(y, x) for y, x in get_coords(battlefield, y, x, FREE, SHIP) if (y, x) not in path and battlefield[y][x] == SHIP]:
         battlefield[y][x] = SCANNED
         path = path + (y, x)
         block = scan_ship(battlefield, path, y, x)
@@ -24,9 +24,9 @@ def scan_ship(battlefield, path, y, x, FREE=0, SHIP=1, SCANNED=3):
     return ship
 
 def explore(battlefield, path, ships=None, FREE=0, SHIP=1, VISITED=2):
-    current = path[-1]
+    y, x = path[-1]
     ships = ships if ships is not None else []
-    for y, x in [yx for yx in get_coords(battlefield, current, FREE, SHIP) if yx not in path]:
+    for y, x in [yx for yx in get_coords(battlefield, y, x, FREE, SHIP) if yx not in path]:
         newpath = path + ((y, x),)
         if battlefield[y][x] == SHIP:
             ships.append(scan_ship(battlefield, newpath, y, x))
