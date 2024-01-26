3ed7b41fb2c83c691255894f83546b1f90197462
atrox3d <atrox3d@gmail.com>
2024-01-26 13:14:43 +0100

    add clearscreen param

--- battleship-field-validator/solution3/main.py
+++ battleship-field-validator/solution3/main.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import logging
 
 import helpers.load as load
 import helpers.matrix as matrix
@@ -6,14 +7,14 @@ import battleship
 
 def main(solution):
     extensions = 'json, ascii, csv'
-    for file_path in load.get_files(extensions=extensions):
+    for file_path in load.get_files('battlefield.ascii'):
         tests = load.battlefield(file_path)
         for test in tests:
             name = test['name']
             battlefield = test['data']
             expected = test['expected']
 
-            matrix.display(battlefield)
+            matrix.display(battlefield, clear_screen=False)
             result = solution(battlefield)
             print(f'{name} -> {result = } -> {expected = }')
             
