9a30f6210733b8767b110c34a54c6fada2fb94bf
atrox3d <atrox3d@gmail.com>
2024-01-26 06:13:18 +0100

    add file extension helper

--- battleship-field-validator/solution3/helpers/loader.py
+++ battleship-field-validator/solution3/helpers/loader.py
@@ -4,8 +4,15 @@ from pathlib import Path
 
 DATA_PATH = Path(__file__).parent.parent / 'data'
 
-def get_files(*wildcards, path=DATA_PATH):
+def parse_extensions(ext_str:str, separator=', '):
+    return tuple([f'*.{ext}' for ext in ext_str.split(sep=separator)])
+
+def get_files(*wildcards, extensions=None, path=DATA_PATH):
     path = Path(path)
+
+    if extensions is not None:
+        wildcards += parse_extensions(extensions)
+
     return [file  for wildcard in [*wildcards] for file in Path(path).glob(wildcard)]
 
 def load_battlefield(file):
@@ -31,7 +38,7 @@ def non_json_adapter(func):
     def wrapper(filename):
         battlefield = func(filename)
         return [{
-                'name': filename,
+                'name': Path(filename).name,
                 'data': battlefield,
                 'expected': 'unknown'
             }]
--- battleship-field-validator/solution3/main.py
+++ battleship-field-validator/solution3/main.py
@@ -5,20 +5,15 @@ import helpers.matrix as matrix
 
 def main(solution):
 
-    JSON = True
-    if JSON:
-        for file in loader.get_files('*.json'):
-            tests = loader.load_battlefield(file)
-            for test in tests:
-                name = test['name']
-                battlefield = test['data']
-                expected = test['expected']
-                matrix.display(battlefield)
-                result = solution(battlefield)
-                print(f'{name} -> {result = } -> {expected = }')
-    else:
-        for file in loader.get_files('*.ascii', '*.csv'):
-            battlefield = loader.load_battlefield(file)
-            battlefield = matrix.convert_battlefield(battlefield)
+    # for file in loader.get_files('*.json', '*.ascii', '*.csv'):
+    for file in loader.get_files(extensions='json, ascii, csv'):
+        tests = loader.load_battlefield(file)
+        for test in tests:
+            name = test['name']
+            battlefield = test['data']
+            expected = test['expected']
+            matrix.display(battlefield)
             result = solution(battlefield)
-            print(f'{file.name} -> {result}')
+            print(f'{name} -> {result = } -> {expected = }')
+            print()
+            print()
