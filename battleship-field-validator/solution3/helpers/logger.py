763cdcf0d626c7b4b446ed4f8e21cf4bc9e7d7cd
atrox3d <atrox3d@gmail.com>
2024-01-26 13:18:48 +0100

    logging

---
+++ battleship-field-validator/solution3/helpers/logger.py
@@ -0,0 +1,19 @@
+import atexit
+import json
+import logging.config
+import logging.handlers
+import pathlib
+
+logger = logging.getLogger("my_app")  # __name__ is a common choice
+
+
+def setup_logging():
+    config_file = pathlib.Path("logging_configs/2-stderr-json-file.json")
+    with open(config_file) as f_in:
+        config = json.load(f_in)
+
+    logging.config.dictConfig(config)
+    queue_handler = logging.getHandlerByName("queue_handler")
+    if queue_handler is not None:
+        queue_handler.listener.start()
+        atexit.register(queue_handler.listener.stop)
