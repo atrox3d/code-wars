6bbd00266356cfcfa8547bd693d01f946dbb2eae
atrox3d <atrox3d@gmail.com>
2024-01-27 08:03:00 +0100

    update_config_value

--- battleship-field-validator/solution3/helpers/logsetup.py
+++ battleship-field-validator/solution3/helpers/logsetup.py
@@ -6,34 +6,39 @@ import pathlib
 
 logger = logging.getLogger(__name__)  # __name__ is a common choice
 
-def override(nested: dict, path: str, value, sep='.'):
-    path = path.split(sep)
-    target = nested
-    for key in path[:-1]:
-        target = target[key]
-    target[path[-1]] = value
+class NestedKeyError(KeyError):
+    pass
 
-def find_path(nested: dict, key: str, path: list=None, sep='.'):
-    path = path or []
-    found = None    
-    for k in nested:
-        if key == k:
-            found = path + [k]
-            break
-        elif isinstance(nested[k], dict):
-            if found := find_path(nested[k], key, path + [k]):
+def update_config_value(config, key, value):
+    def set_nested_value(nested: dict, path: str, value, sep='.'):
+        path = path.split(sep)
+        target = nested
+        for key in path[:-1]:
+            target = target[key]
+        target[path[-1]] = value
+
+    def path_to_key(nested: dict, key: str, path: list=None, sep='.'):
+        path = path or []
+        found = None    
+        for k in nested:
+            if key == k:
+                found = path + [k]
                 break
-    else:
-        if not path: # root of dict, path is []
-            raise KeyError(f'key {key!r} not found')
+            elif isinstance(nested[k], dict):
+                if found := path_to_key(nested[k], key, path + [k]):
+                    break
+        else:
+            if not path: # root of dict, path is []
+                raise NestedKeyError(f'key {key!r} not found')
 
-    if found and not path: # root of dict, path is []
-        if sep: 
-            # return 'sep' separated string
-            return sep.join(found)
-    # return list
-    return found
-
+        if found and not path: # root of dict, path is []
+            if sep: 
+                # return 'sep' separated string
+                return sep.join(found)
+        # return list
+        return found
+    path = path_to_key(config, key)
+    set_nested_value(config, path, value)
 
 def setup(**kwargs):
     config_file = pathlib.Path(__file__).parent / 'logger.json'
@@ -41,9 +46,8 @@ def setup(**kwargs):
         config = json.load(f_in)
 
     for key, value in kwargs.items():
-        path = find_path(config, key)
-        override(config, path, value)
-    
+        update_config_value(config, key, value)
+
     logging.config.dictConfig(config)
     # queue_handler = logging.getHandlerByName("queue_handler")
     # if queue_handler is not None:
