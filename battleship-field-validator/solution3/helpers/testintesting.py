a6463905862a4989100c7d7946a7525efd095c52
atrox3d <atrox3d@gmail.com>
2024-01-28 06:08:41 +0100

    move to separate files

--- battleship-field-validator/solution3/helpers/testintesting.py
+++ battleship-field-validator/solution3/helpers/testintesting.py
@@ -2,70 +2,12 @@ import json
 import logging.config
 import pathlib
 
+import dictutil
+import recursion
+import nested
+
 logger = logging.getLogger(__name__)  # __name__ is a common choice
 
-class NestedKeyError(KeyError):
-    pass
-
-def set_nested_value(nested: dict, path: str, value, sep='.'):
-    if isinstance(path, str):
-        path = path.split(sep)
-    target = nested
-    for key in path[:-1]:
-        target = target[key]
-    target[path[-1]] = value
-
-def path_to_key(nested: dict, key: str, path: list=None, sep='.'):
-    path = path or []
-    if isinstance(path, str):
-        path = path.split(sep)
-        # *path, key= key.split(sep)
-        start = nested
-        for node in path:
-            start = start[node]
-        
-        print(f'{start = }, {path = }, {key = }')
-        found =  path_to_key(start, key, path)
-        if not found:
-            raise NestedKeyError(f'path_to_key: key {key!r} not found {path = }')
-        else:
-            return found
-    
-    found = None
-    for k in nested:
-        if key == k:
-            found = path + [k]
-            break
-        elif isinstance(nested[k], dict):
-            if found := path_to_key(nested[k], key, path + [k]):
-                break
-    else:
-        if not path: # root of dict, path is []
-            raise NestedKeyError(f'path_to_key: key {key!r} not found {path = }')
-
-    if found and not path: # root of dict, path is []
-        if sep: 
-            # return 'sep' separated string
-            return sep.join(found)
-    # return list
-    return found
-
-def find(d, key, keys=None):
-    root = keys is None
-    keys = keys or []
-    for k, v in d.items():
-        if k == key:
-            return keys + [k]
-        elif isinstance(v, dict):
-            if keys := find(v, key, keys + [k]):
-                return keys
-    if not root:
-        return []
-    raise NestedKeyError(f'find: key {key!r} not found {keys = }')
-
-def update_config_value(config, key, value):
-    path = path_to_key(config, key)
-    set_nested_value(config, path, value)
 
 def setup(**kwargs):
     config_file = pathlib.Path(__file__).parent / 'logger.json'
@@ -73,24 +15,15 @@ def setup(**kwargs):
         config = json.load(f_in)
 
     for key, value in kwargs.items():
-        update_config_value(config, key, value)
+        nested.update_config_value(config, key, value)
 
     logging.config.dictConfig(config)
     return config
-
-def traverse(d, keys=None):
-    keys = keys or []
-    for k, v in d.items():
-        keys.append(k)
-        print(k)
-        if isinstance(v, dict):
-            traverse(v, keys)
-    return keys
         
 if __name__ == '__main__':
     config = setup()
     print(json.dumps(config, indent=1))
-    keys = traverse(config)
+    keys = recursion.traverse(config)
     keys.append('error man!')
     print(keys)
     # print(find(config, 'stream'))
@@ -99,13 +32,13 @@ if __name__ == '__main__':
     # exit()
     for key in keys:
         try:
-            found = find(config, key)
+            found = dictutil.find(config, key)
             print(f'{found = }')
-        except NestedKeyError as ne:
+        except dictutil.NestedKeyError as ne:
             print(repr(ne))
         try:
-            pathtokey = path_to_key(config, key)
+            pathtokey = nested.path_to_key(config, key)
             print(f'{pathtokey = }')
-        except NestedKeyError as ne:
+        except dictutil.NestedKeyError as ne:
             print(repr(ne))
         print()
