72240d6e6610ebf3b8faba3e18d3c7330c6f0a37
atrox3d <atrox3d@gmail.com>
2024-01-28 07:48:47 +0100

    change

--- battleship-field-validator/solution3/helpers/log.py
+++ battleship-field-validator/solution3/helpers/log.py
@@ -4,13 +4,13 @@ import pathlib
 
 import nested
 
-def setup(change_value=nested.update_config_value, *args, **kwargs):
+def setup(change=nested.update_config_value, *args, **kwargs):
     config_file = pathlib.Path(__file__).parent / 'logger.json'
     with open(config_file) as f_in:
         config = json.load(f_in)
 
     for key, value in kwargs.items():
-        nested.update_config_value(config, key, value)
+        change(config, key, value)
 
     logging.config.dictConfig(config)
     return config
